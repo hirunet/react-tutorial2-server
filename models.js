c404a89d919382458a34d65e5c6b8665ae26cb5a
H.Komine <hk0mine@outlook.jp>
2021-01-09 23:33:29 +0900

    Update: psqlに接続する際のSSLに関するエラーに対応

--- models.js
+++ models.js
@@ -4,7 +4,14 @@ const { DataTypes } = Sequelize;
 
 const url = process.env.DATABASE_URL;
 
-export const sequelize = new Sequelize(url);
+export const sequelize = new Sequelize(url, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 export const User = sequelize.define(
   "user",
